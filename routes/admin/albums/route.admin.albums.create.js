9f173c52bc26483ffa6d1870cee5af0711484428
maximeMD <maxime.martydessus@gmail.com>
2018-06-17 19:35:24 +0200

    check album name on create

--- routes/admin/albums/route.admin.albums.create.js
+++ routes/admin/albums/route.admin.albums.create.js
@@ -1,13 +1,19 @@
 const Album = require('../../../app/models/model.album');
 
 module.exports = (req, res) => {
-  var newAlbum = new Album({
-    "name": req.body.albumName,
-    "src_thumbnail" : "0"
-  });
-  newAlbum.save(function(err){
-    if (err) throw err;
-    console.log("Album save success");
-  });
-  res.redirect('/admin/albums');
+  // Check if the album name is Alphanumeric
+  if(/^([0-9]|[a-z])+([0-9a-z]+)$/i.test(req.body.albumName)){
+    var newAlbum = new Album({
+      "name": req.body.albumName,
+      "src_thumbnail" : "0"
+    });
+    newAlbum.save(function(err){
+      if (err) throw err;
+      console.log("Album save success");
+    });
+    res.status(201).send('Album created with name : ' + newAlbum.name);
+  }
+  else{
+    res.status(400).send("Album name must be composed with alphanumeric-only characters")
+  }
 };
