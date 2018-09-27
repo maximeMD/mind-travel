b29dce0fe05662ef9951006249ecd36ba9a4a255
maximeMD <maxime.martydessus@gmail.com>
2018-09-27 23:29:20 +0200

    [FEAT] admin/albums front

--- routes/admin/albums/route.admin.albums.all.js
+++ routes/admin/albums/route.admin.albums.all.js
@@ -2,6 +2,9 @@ const Album = require('../../../app/models/model.album');
 
 module.exports = (req, res) => {
   Album.find({}, function(err, albums){
+    // Image.aggregate([
+    //   {"$group" : {_id:"$source", count:{$sum:1}}}
+    // ])    
     res.render('admin/pages/albums/all', {
       user : req.user,
       albums: albums
