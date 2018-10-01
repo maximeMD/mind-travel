cbfe386cd3161f5395a512e04fae3e01b507104d
Maxime Marty-Dessus <maxime.marty-dessus@ext.soprasteria.com>
2018-10-01 14:14:21 +0200

    first try catch

--- routes/admin/images/route.admin.images.delete.js
+++ routes/admin/images/route.admin.images.delete.js
@@ -6,26 +6,29 @@ var settings = require('../../../config/config.settings');
 const del = require('del');
 
 module.exports = (req, res) => {
-
-  Image.findById(req.body.imageId, function(err, image){
-    // if(err) throw err;
-    Album.findById(image.album, function(err, album){
+  try{    
+    Image.findById(req.body.imageId, function(err, image){
       // if(err) throw err;
-      if(album.src_thumbnail == image.src_thumbnail){
-        Image.findOne({'album' : album.id}, function(err,newImageThumb){
-          // if(err) throw err;
-          Album.findByIdAndUpdate(album.id, {"src_thumbnail":newImageThumb.src_thumbnail}, function(err, result){
-            console.log("Album thumbnail changed by first image");
+      Album.findById(image.album, function(err, album){
+        // if(err) throw err;
+        if(album.src_thumbnail == image.src_thumbnail){
+          Image.findOne({'album' : album.id}, function(err,newImageThumb){
+            // if(err) throw err;
+            Album.findByIdAndUpdate(album.id, {"src_thumbnail":newImageThumb.src_thumbnail}, function(err, result){
+              console.log("Album thumbnail changed by first image");
+            });
           });
-        });
-      }
+        }
+      });
+      Image.findByIdAndRemove(image.id, function(err){
+        if(err) throw err;
+        console.log("Image removed from database");
+        del(settings.pathAlbums+req.body.albumName+"/"+req.body.imageSrc).then(function(){console.log("Image file removed");});
+        del(settings.pathThumbnails+req.body.albumName+"/"+req.body.imageSrcThumb).then(function(){console.log("Image thumbnail file removed");});
+      });
     });
-    Image.findByIdAndRemove(image.id, function(err){
-      if(err) throw err;
-      console.log("Image removed from database");
-      del(settings.pathAlbums+req.body.albumName+"/"+req.body.imageSrc).then(function(){console.log("Image file removed");});
-      del(settings.pathThumbnails+req.body.albumName+"/"+req.body.imageSrcThumb).then(function(){console.log("Image thumbnail file removed");});
-    });
-  });
-  res.redirect("/admin/albums/"+req.body.albumName);
+    res.redirect("/admin/albums/"+req.body.albumName);
+  } catch (error) {
+    res.status(500).json({error: error.toString()});
+  }
 };
--- routes/admin/images/route.admin.images.upload.js
+++ routes/admin/images/route.admin.images.upload.js
@@ -10,65 +10,69 @@ var mime = require('mime');
 var settings = require('../../../config/config.settings');
 
 module.exports = (req, res) => {
-  //find the corresponding Album in database
-
-  Album.findById(req.params.id, function(err, album){
-    if (err) throw err;
-
-    var dirName = settings.pathAlbums+album.id;
-    //create upload folders if it doesn't exists
-    if (!fs.existsSync(dirName)){
-        fs.mkdirSync(dirName);
-    }
-    if (!fs.existsSync(settings.pathThumbnails+album.id)){
-        fs.mkdirSync(settings.pathThumbnails+album.id);
-    }
-  
-    //define the storage for multer (upload module)
-    var storage =   multer.diskStorage({
-      destination: function (req, file, callback) {
-        callback(null, dirName);
-      },
-      filename: function (req, file, callback) {
-        callback(null, album.name + "_" + Math.random().toString(36).substr(2, 9) + "." + mime.getExtension(file.mimetype));
-      }
-    });
-    //upload the files
-    var upload = multer({ storage : storage }).array('userPhoto',1000);
-    upload(req,res,function(err){
+  try{
+    //find the corresponding Album in database
+    Album.findById(req.params.id, function(err, album){
       if (err) throw err;
-      req.files.forEach(function(image){
-        //upload to database
-        var dimensions = sizeOf(dirName+'/'+image.filename);
-        var img_doc = new Image({
-          src: image.filename.split("."+mime.getExtension(image.mimetype),1) + "." + mime.getExtension(image.mimetype),
-          src_thumbnail: image.filename.split("."+mime.getExtension(image.mimetype),1) +'_thumb' + "." + mime.getExtension(image.mimetype),
-          width: dimensions.width,
-          height: dimensions.height,
-          album: album.id
-        });
-        img_doc.save(function (err) {
-          if (err) return handleError(err);
-          console.log(image.filename + ' : Image saved');
-        });
-        // create thumb
-        thumb({
-          source: dirName +'/'+image.filename,
-          destination: settings.pathThumbnails+album.id
-        }, function(files, err, stdout, stderr) {
-          console.log(image.filename + ' : Thumb created');
-        });
-        // set thumbnail to the album if it hasn't one yet
-        if(album.src_thumbnail === "0"){
-          album.src_thumbnail = image.filename.split("."+mime.getExtension(image.mimetype),1) +'_thumb' + "." + mime.getExtension(image.mimetype);
-          Album.findByIdAndUpdate(album.id, album, function(err, updatedAlbum){
-            if (err) throw err;
-            console.log("Thumbnail added to album " + updatedAlbum.name);
-          });
+      
+      var dirName = settings.pathAlbums+album.id;
+      //create upload folders if it doesn't exists
+      if (!fs.existsSync(dirName)){
+        fs.mkdirSync(dirName);
+      }
+      if (!fs.existsSync(settings.pathThumbnails+album.id)){
+        fs.mkdirSync(settings.pathThumbnails+album.id);
+      }
+      
+      //define the storage for multer (upload module)
+      var storage =   multer.diskStorage({
+        destination: function (req, file, callback) {
+          callback(null, dirName);
+        },
+        filename: function (req, file, callback) {
+          callback(null, album.name + "_" + Math.random().toString(36).substr(2, 9) + "." + mime.getExtension(file.mimetype));
         }
       });
-      res.redirect("/admin/albums/"+album.id);
+      //upload the files
+      var upload = multer({ storage : storage }).array('userPhoto',1000);
+      upload(req,res,function(err){
+        if (err) throw err;
+        req.files.forEach(function(image){
+          //upload to database
+          var dimensions = sizeOf(dirName+'/'+image.filename);
+          var img_doc = new Image({
+            src: image.filename.split("."+mime.getExtension(image.mimetype),1) + "." + mime.getExtension(image.mimetype),
+            src_thumbnail: image.filename.split("."+mime.getExtension(image.mimetype),1) +'_thumb' + "." + mime.getExtension(image.mimetype),
+            width: dimensions.width,
+            height: dimensions.height,
+            album: album.id
+          });
+          img_doc.save(function (err) {
+            if (err) return handleError(err);
+            console.log(image.filename + ' : Image saved');
+          });
+          // create thumb
+          thumb({
+            source: dirName +'/'+image.filename,
+            destination: settings.pathThumbnails+album.id
+          }, function(files, err, stdout, stderr) {
+            console.log(image.filename + ' : Thumb created');
+          });
+          // set thumbnail to the album if it hasn't one yet
+          if(album.src_thumbnail === "0"){
+            album.src_thumbnail = image.filename.split("."+mime.getExtension(image.mimetype),1) +'_thumb' + "." + mime.getExtension(image.mimetype);
+            Album.findByIdAndUpdate(album.id, album, function(err, updatedAlbum){
+              if (err) throw err;
+              console.log("Thumbnail added to album " + updatedAlbum.name);
+            });
+          }
+        });
+        res.redirect("/admin/albums/"+album.id);
+      });
     });
-  });
-
+  } catch (error) {
+    res.status(500).json({error: error.toString()});
+  }
+    
 };
+  
